346acad3b4e9e2edfb67de2cc10c8634ac07687b
sujoysuktoemon <sujoysuktoemon@gmail.com>
2014-10-30 23:21:51 +0600

    FileOparationApp

---
+++ FileOparationApps/FileOparationApps/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FileOparationApps
+{
+    partial class FileOparationApps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEnterName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.EnterNameListBox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter 1st Name";
+            // 
+            // txtEnterName
+            // 
+            this.txtEnterName.Location = new System.Drawing.Point(110, 27);
+            this.txtEnterName.Name = "txtEnterName";
+            this.txtEnterName.Size = new System.Drawing.Size(138, 20);
+            this.txtEnterName.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(162, 80);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(162, 109);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // EnterNameListBox
+            // 
+            this.EnterNameListBox.FormattingEnabled = true;
+            this.EnterNameListBox.Location = new System.Drawing.Point(44, 138);
+            this.EnterNameListBox.Name = "EnterNameListBox";
+            this.EnterNameListBox.Size = new System.Drawing.Size(193, 95);
+            this.EnterNameListBox.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter 2nd Name";
+            // 
+            // FileOparationApps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EnterNameListBox);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtEnterName);
+            this.Controls.Add(this.label1);
+            this.Name = "FileOparationApps";
+            this.Text = "File Oparation Apps";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEnterName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.ListBox EnterNameListBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FileOparationApps/FileOparationApps/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileOparationApps
+{
+    public partial class FileOparationApps : Form
+    {
+        public FileOparationApps()
+        {
+            InitializeComponent();
+        }
+
+        private string fileLocation = @"B:/emon.txt";
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream aFileStream=new FileStream(fileLocation,FileMode.Append);
+            StreamWriter aStreamWriter = new StreamWriter(aFileStream);
+            aStreamWriter.Write(txtEnterName.Text + "_");
+            aStreamWriter.Write(textBox1.Text);
+
+            aStreamWriter.WriteLine();
+            aStreamWriter.Close();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            FileStream aFileStream=new FileStream(fileLocation,FileMode.Open);
+            StreamReader aStreamReader=new StreamReader(aFileStream);
+            EnterNameListBox.Items.Clear();
+            while (!aStreamReader.EndOfStream)
+            {
+                string aLine = aStreamReader.ReadLine();
+                EnterNameListBox.Items.Add(aLine);
+            }
+            aStreamReader.Close();
+        }
+    }
+}
